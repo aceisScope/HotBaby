96e37dbc2db70ba7b3002ffb3b72934f12da0ee3
aceisScope <bertieliu52@gmail.com>
2015-11-29 18:40:00 +0200

    add several methods for batterary draining

--- HotBaby/app/src/main/java/hut/hotbaby/MainActivity.java
+++ HotBaby/app/src/main/java/hut/hotbaby/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    public static Camera camera = null;// has to be static, otherwise onDestroy() destroys it
+    private RegexThreads[] regexThreads;
+    private NetworkTask[] networkTasks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,4 +48,108 @@ public class MainActivity extends Activity {
         layout.screenBrightness = 1F;
         getWindow().setAttributes(layout);
     }
+
+    private void startLocation() {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {}
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        for (String provider : locationManager.getAllProviders()) {
+            locationManager.requestLocationUpdates(provider, 0, 0, locationListener);
+        }
+
+    }
+
+    // deprecated from lollipop
+    private void flashLightOn() {
+
+        try {
+            if (getPackageManager().hasSystemFeature(
+                    PackageManager.FEATURE_CAMERA_FLASH)) {
+                cam = Camera.open();
+                Parameters p = cam.getParameters();
+                p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                cam.setParameters(p);
+                cam.startPreview();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Exception flashLightOn()",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // http://stackoverflow.com/questions/7396766/how-can-i-stress-my-phones-cpu-programatically
+    private void burnCPU() {
+        static int NUM_THREADS = 10; // run 10 threads
+
+        regexThreads = new RegexThread [NUM_THREADS];
+
+        for(int i = 0; i < NUM_THREADS; ++i) {
+            RegexThread[i] = new RegexThread(); // create a new thread
+        }
+    }
+
+    //-------------------------------------------------
+    // turn off
+    private void flashLightOff(View view) {
+        try {
+            if (getPackageManager().hasSystemFeature(
+                    PackageManager.FEATURE_CAMERA_FLASH)) {
+                cam.stopPreview();
+                cam.release();
+                cam = null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Exception flashLightOff",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(this);
+    }
+
+    private void stopCPU() {
+        for (int i = 0; i < regexThreads.length; i++) {
+            RegexThread[i].terminate();
+        }
+    }
+
+    //-----helper class-----------------------------------
+    class RegexThread extends Thread {
+
+        private volatile boolean running = true;
+
+        RegexThread() {
+            // Create a new, second thread
+            super("Regex Thread");
+            start(); // Start the thread
+        }
+
+        // This is the entry point for the second thread.
+        public void run() {
+            while(running) {
+                Pattern p = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b");
+            }
+        }
+
+        public void terminate() {
+            running = false;
+        }
+    }
+
 }
